3cc252ccd31d5a2dfa3bfdd66bc8a8087fff1c5c
Hanna <kovalenkoas1984@gmail.com>
2023-11-09 22:48:22 +0300

    Add the hw with triangle

---
+++ file1.js
@@ -0,0 +1,19 @@
+function triangle (height, sign) {
+    let y = " ";
+    for (let i = 0; i < height; i++) {
+     y = y + sign; console.log(y);
+    }
+    }
+    triangle(10, "*")
+    
+    function triangle (high,sign) {
+    let y = " "; 
+    let i = 0;
+    do {
+    y = y + sign; 
+    i++;
+    console.log(y); 
+    }
+    while (i <height)} 
+    triangle (10, "*")
+    
