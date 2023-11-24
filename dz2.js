acbc34ff51f286c560c4055ed3a7c70ec66fef8f
Hanna <kovalenkoas1984@gmail.com>
2023-11-24 10:13:34 +0200

    Add file with second homework

---
+++ dz2.js
@@ -0,0 +1,60 @@
+
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+
+function findValueByKey(companyObject, companyName) {
+  if (companyObject.name === companyName) {
+    console.log("Найден объект:", companyObject);
+  } else if (Array.isArray(companyObject.partners)) {
+    companyObject.partners.forEach((partner) => {
+      findValueByKey(partner, companyName);
+    });
+  } else if (Array.isArray(companyObject.clients)) {
+    companyObject.clients.forEach((client) => {
+      findValueByKey(client, companyName);
+    });
+  } 
+};
+findValueByKey(company,'Клієнт 2');
+findValueByKey(company,'Клієнт 1.2.3');
